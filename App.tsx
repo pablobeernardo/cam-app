bc96905d0cb6794d4f658205b7b4d3b6948b7e54
pablobeernardo <pablobeernardo@gmail.com>
2023-05-29 21:34:45 -0300

    Adicionando arquivos

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from "expo-camera";
+import { useEffect, useState } from "react";
+import { Button, View, StyleSheet } from "react-native";
+import MediaLibrary from 'expo-media-library';
 
-export default function App() {
-  return (
+export default function App(){
+
+  const [image, setImage] = useState(null);
+  const [camera, setCamera] = useState<Camera>(null);
+  const [permission, setPermission] = useState(null);
+  const [capturedimage, setCapturedimage] = useState(null);
+
+
+  useEffect(() =>{
+
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setPermission(cameraStatus.status === 'granted');
+    })();
+  },[]);
+
+  async function takePicture(){
+    if (camera) {
+
+    const photo = await camera.takePictureAsync();
+    console.log(photo.uri)
+
+    await MediaLibrary.saveToLibraryAsync(photo.uri);
+
+    }
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera 
+        ref={(ref) =>setCamera(ref)}
+        style={styles.styleCamera}
+        type={CameraType.back}
+        ratio={'1:1'}
+      />
+      <Button title="Capturar Foto" onPress={() => {takePicture()}} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1
   },
+
+  styleCamera: {
+    aspectRatio: 1,
+    flex: 1
+  }
+
 });
